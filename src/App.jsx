e1310b970e255d1703d50b33cde3ed96fdf83da7
RahulB <rahulbisht1012@gmail.com>
2024-01-28 15:28:44 +0530

    using react table

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable */
+import React from "react";
+import {useTable, useSortBy, usePagination} from "react-table";
+import {data} from "./assets/data.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const columns = [
+    {
+        Header: "ID",
+        accessor: "id",
+    },
+    {
+        Header: "NAME",
+        accessor: "name",
+    },
+    {
+        Header: "DEPARTMENT",
+        accessor: "department",
+    },
+    {
+        Header: "SALARY",
+        accessor: "salary",
+    },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const {getTableProps, getTableBodyProps, headerGroups, rows, page, prepareRow, nextPage, previousPage} = useTable(
+        {
+            columns,
+            data,
+        },
+        useSortBy,
+        usePagination
+    );
 
-export default App
+    return (
+        <>
+            <div className="container">
+                <table {...getTableProps()}>
+                    <thead>
+                        {headerGroups.map((hg) => (
+                            <tr {...hg.getHeaderGroupProps()}>
+                                {hg.headers.map((column) => (
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                        {column.render("Header")}
+                                        {column.isSorted && <span>{column.isSortedDesc ? "^" : "$"}</span>}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {page.map((row) => {
+                            prepareRow(row);
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map((cell) => (
+                                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                                    ))}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+                <div>
+                    <button onClick={previousPage}>Previous</button>
+                    <button onClick={nextPage}>Next</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
